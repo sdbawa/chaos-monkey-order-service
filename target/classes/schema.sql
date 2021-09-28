dba157abe4257890eeac0d1a1b1c9de814665316
Simar Bawa <45673906+sdbawa@users.noreply.github.com>
2021-09-27 17:57:51 -0700

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
---DROP TABLE IF EXISTS order;
-
-CREATE TABLE ORDERS (
-                               id INT PRIMARY KEY,
-                               status VARCHAR(50) NOT NULL
-);
-
-CREATE SEQUENCE incrOneSeq START WITH 1 INCREMENT BY 1;
